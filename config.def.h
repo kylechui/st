39b65fccdf97473dae021afc3d18fc0a15084947
Kyle Chui <kyle.chui+github@pm.me>
2024-03-18 16:39:10 -0700

    feat: Set default font size to 14.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:size=12";
+static char *font = "Iosevka:size=14";
 static int borderpx = 2;
 
 /*
