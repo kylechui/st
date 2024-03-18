c22b829b421b7af662dbd1c3eea8d1053e737a39
Kyle Chui <kyle.chui+github@pm.me>
2024-03-18 16:22:53 -0700

    feat: Theme the terminal.

--- config.def.h
+++ config.def.h
@@ -96,32 +96,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#090618",
+	"#c34043",
+	"#76946a",
+	"#c0a36e",
+	"#7e9cd8",
+	"#957fb8",
+	"#6a9589",
+	"#c8c093",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#727169",
+	"#e82424",
+	"#98bb6c",
+	"#e6c384",
+	"#7fb4ca",
+	"#938aa9",
+	"#7aa89f",
+	"#dcd7ba",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#dcd7ba", /* default foreground colour */
+	"#1f1f28", /* default background colour */
 };
 
 
@@ -131,7 +131,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
